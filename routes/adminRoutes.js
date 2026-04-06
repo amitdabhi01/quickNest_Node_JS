0db29c6adb578795f285093ec0f12c44f43c203a
amitdabhi01 <amitdabhi526@gmail.com>
2026-04-06 11:09:31 +0530

    added category controller

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,7 +2,9 @@ import express from "express";
 
 import auth from "../middleware/auth.js";
 import checkRole from "../middleware/checkRole";
+
 import adminController from "../controller/adminController.js";
+import categoryController from "../controller/categoryController.js";
 
 const router = express.Router();
 
@@ -18,6 +20,13 @@ router.delete(
   auth,
   checkRole("admin", "super_admin"),
   adminController.deleteUser,
+);
+
+router.post(
+  "/add",
+  auth,
+  checkRole("admin", "super_admin"),
+  categoryController.add,
 );
 
 export default router;
