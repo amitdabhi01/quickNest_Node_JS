5ae6fefb86acb6a5eeb60019d4024d97642f6419
amitdabhi01 <amitdabhi526@gmail.com>
2026-04-07 14:32:11 +0530

    service model and controller created

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -5,9 +5,15 @@ import checkRole from "../middleware/checkRole.js";
 
 import adminController from "../controller/adminController.js";
 import categoryController from "../controller/categoryController.js";
+import serviceController from "../controller/serviceController.js";
+
+import validate from "../middleware/validate.js";
+
+import { createCategorySchema } from "../validation/categorySchema.js";
 
 const router = express.Router();
 
+// user
 router.patch(
   "/update/:id",
   auth,
@@ -22,11 +28,21 @@ router.delete(
   adminController.deleteUser,
 );
 
+// category
 router.post(
   "/addCategory",
   auth,
+  validate(createCategorySchema),
   checkRole("admin", "super_admin"),
   categoryController.add,
+);
+
+// service
+router.post(
+  "/addService",
+  auth,
+  checkRole("admin", "super_admin"),
+  serviceController.add,
 );
 
 export default router;
