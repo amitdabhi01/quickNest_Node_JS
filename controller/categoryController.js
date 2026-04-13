23a1bd6d89cb3eec89e66240148e5de22f4024d0
amitdabhi01 <amitdabhi526@gmail.com>
2026-04-13 12:40:32 +0530

    category delte controller added

--- controller/categoryController.js
+++ controller/categoryController.js
@@ -55,10 +55,30 @@ const getById = async (req, res, next) => {
 
 const update = async (req, res, next) => {
   try {
-    
   } catch (error) {
     next(new HttpError(error.message, 500));
   }
 };
 
-export default { add, getAll, getById, update };
+const deleteCategory = async (req, res, next) => {
+  try {
+    const { id } = req.params.id;
+
+    const category = await Category.findById(id);
+
+    if (!category) {
+      return next(new HttpError("category not found", 404));
+    }
+
+    await Category.findByIdAndDelete(id);
+
+    res.status(200).json({
+      success: true,
+      message: "category deleted successfully",
+    });
+  } catch (error) {
+    next(new HttpError(error.message, 500));
+  }
+};
+
+export default { add, getAll, getById, update, deleteCategory };
