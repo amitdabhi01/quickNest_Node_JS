0d154e6b6507f1d3f4d313828f9e3006c7e58d64
amitdabhi01 <amitdabhi526@gmail.com>
2026-04-17 11:46:28 +0530

    booking model created

--- model/Booking.js
+++ model/Booking.js
@@ -1,10 +1,53 @@
+import { required, types } from "joi";
 import mongoose from "mongoose";
 
-const bookingSchema = new mongoose.Schema({
-  userId: {
-    type:mongoose.       
+const bookingSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    serviceId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Service",
+      required: true,
+    },
+    bookingDate: {
+      type: Date,
+      required: true,
+    },
+    timeSlot: {
+      type: String,
+      enum: [
+        "9:00-10:00",
+        "10:00-11:00",
+        "11:00-12:00",
+        "12:00-13:00",
+        "13:00-14:00",
+        "14:00-15:00",
+        "15:00-16:00",
+        "16:00-17:00",
+        "17:00-18:00",
+      ],
+      required: true,
+    },
+    notes: {
+      type: String,
+      maxLength: 50,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "confirmed", "complete", "cancelled"],
+      default: "pending",
+    },
+    totalPrice: {
+      type: Number,
+      required: true,
+    },
   },
-});
+  { timestamps: true },
+);
 
 const Booking = mongoose.model("Booking", bookingSchema);
 
