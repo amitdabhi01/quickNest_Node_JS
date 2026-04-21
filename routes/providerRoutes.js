71a2cdfb292421596c5caf40bb3f8fe6c7beb98e
amitdabhi01 <amitdabhi526@gmail.com>
2026-04-21 12:30:55 +0530

    booking routes completed

--- routes/providerRoutes.js
+++ routes/providerRoutes.js
@@ -1,7 +1,6 @@
 import express from "express";
 
 import auth from "../middleware/auth.js";
-
 import providerController from "../controller/providerController.js";
 
 const router = express.Router();
