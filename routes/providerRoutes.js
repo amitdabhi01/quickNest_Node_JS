8fd1e2bae4dda0308cee4bf2117187f25f9bbadd
amitdabhi01 <amitdabhi526@gmail.com>
2026-04-21 12:53:05 +0530

    getproviders routes

--- routes/providerRoutes.js
+++ routes/providerRoutes.js
@@ -1,10 +1,18 @@
 import express from "express";
 
 import auth from "../middleware/auth.js";
+import checkRole from "../middleware/checkRole.js";
 import providerController from "../controller/providerController.js";
 
 const router = express.Router();
 
 router.post("/register", auth, providerController.registerAsProvider);
+
+router.get(
+  "/providers",
+  auth,
+  checkRole("admin", "super_admin"),
+  providerController.getProviders,
+);
 
 export default router;
