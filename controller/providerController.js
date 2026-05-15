44fa96587810a87e5a55602b3928699eb1a24cb8
amitdabhi01 <amitdabhi526@gmail.com>
2026-05-15 17:12:23 +0530

    getProviderById or GetBookingProvider added

--- controller/providerController.js
+++ controller/providerController.js
@@ -3,6 +3,7 @@ import HttpError from "../middleware/HttpError.js";
 import Provider from "../model/Provider.js";
 import Service from "../model/Service.js";
 import User from "../model/User.js";
+import Booking from "../model/Booking.js";
 
 import sendEmail from "../utils/sendEmail.js";
 import { getProviderRegistrationEmailTemplate } from "../services/emailTemplate.js";
@@ -98,6 +99,63 @@ const getProviders = async (req, res, next) => {
   }
 };
 
+const getProviderById = async (req, res, next) => {
+  try {
+    const id = req.params.id;
 
+    const provider = await Provider.findById(id);
 
-export default { registerAsProvider, getProviders };
+    if (!provider) {
+      return next(new HttpError("provider not found", 404));
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "provider fetched successfully",
+      provider,
+    });
+  } catch (error) {
+    next(new HttpError(error.message, 500));
+  }
+};
+
+const getBookingProvider = async (req, res, next) => {
+  try {
+    const role = req.user.role;
+
+    let bookings;
+
+    if (role === "provider") {
+      const provider = await Provider.findOne({ userId: req.user._id });
+
+      if (!provider) {
+        return next(new HttpError("Provider not found", 404));
+      }
+
+      bookings = await Booking.find({ providerId: provider._id });
+    } else if (role === "admin" || role === "super_admin") {
+      bookings = await Booking.find(); // admin gets all bookings
+    }
+
+    if (!bookings || bookings.length === 0) {
+      return next(new HttpError("No bookings found", 404));
+    }
+
+    res
+      .status(200)
+      .json({
+        success: true,
+        message: "Booking fetched successfully",
+        bookings,
+      });
+  } catch (error) {
+    next(new HttpError(error.message, 500));
+  }
+};
+
+export default {
+  registerAsProvider,
+  getProviders,
+  getProviderById,
+  getBookingProvider,
+};
